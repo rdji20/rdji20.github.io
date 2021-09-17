354bc3665a2db21c7c4bfc34d81c4138645379c6
Roberto <roberto.infante03@gmail.com>
2021-09-17 13:48:09 -0700

    UX upgrade

--- index.js
+++ index.js
@@ -7,9 +7,25 @@
     id("menu").addEventListener("click", showMenuScreen);
     id("exit-icon").addEventListener("click", showScreen);
     id("ame-btn").addEventListener("click", getAboutMe);
+    id("work-btn").addEventListener("click", getWork);
+    id("contact-btn").addEventListener("click", getContactInfo);
   }
 
-  function getAboutMe() {}
+  function getContactInfo() {
+    id("contact").classList.remove("hidden");
+    id("menu_links").classList.remove("unscrolled");
+  }
+
+  function getAboutMe() {
+    id("menu_links").classList.remove("unscrolled");
+    id("about-me").classList.remove("hidden");
+  }
+
+  function getWork() {
+    id("main_screen").classList.remove("hidden");
+    id("menu").classList.remove("hidden");
+    id("menu_links").classList.remove("unscrolled");
+  }
 
   function showScreen() {
     id("main_screen").classList.remove("hidden");
