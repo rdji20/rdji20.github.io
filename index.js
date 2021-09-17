a7de09b46947fbae0da7be0245198528a565d221
Roberto <roberto.infante03@gmail.com>
2021-09-16 23:52:33 -0700

    MADE CHANGES IN ui

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 "use strict";
+
 (function () {
   window.addEventListener("load", init);
 
   function init() {
     id("menu").addEventListener("click", showMenuScreen);
     id("exit-icon").addEventListener("click", showScreen);
+    id("ame-btn").addEventListener("click", getAboutMe);
   }
+
+  function getAboutMe() {}
 
   function showScreen() {
     id("main_screen").classList.remove("hidden");
     id("menu").classList.remove("hidden");
-    id("menu_links").classList.add("hidden");
+    id("menu_links").classList.remove("unscrolled");
   }
 
   function showMenuScreen() {
     id("main_screen").classList.add("hidden");
     id("menu").classList.add("hidden");
-    id("menu_links").classList.remove("hidden");
+    id("menu_links").classList.add("unscrolled");
   }
 
   /**
